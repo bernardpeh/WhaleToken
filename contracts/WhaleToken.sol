37a0c1e061247e410a0e04f6b8b48c0d5ca5c90f
Bernard Peh <bpeh777@gmail.com>
2018-01-15 17:08:43 +1100

    incl. build files

--- contracts/WhaleToken.sol
+++ contracts/WhaleToken.sol
@@ -12,17 +12,17 @@ contract WhaleToken is StandardToken {
 
   string public constant name = "WhaleToken";
   string public constant symbol = "WHALE";
-  uint8 public constant decimals = 18;
+  uint public constant decimals = 18;
 
-  uint256 public constant INITIAL_SUPPLY = 500000 * (10 ** uint256(decimals));
-
+  uint public constant INITIAL_SUPPLY = 500000 * (10 ** uint256(decimals));
+  address public constant owner = 0x69c168f4A372e52fE51A4435638a9a893B3Ac9D1;
   /**
    * @dev Constructor that gives msg.sender all of existing tokens.
    */
   function WhaleToken() public {
     totalSupply = INITIAL_SUPPLY;
-    balances[msg.sender] = INITIAL_SUPPLY;
-    Transfer(0x0, msg.sender, INITIAL_SUPPLY);
+    balances[owner] = INITIAL_SUPPLY;
+    Transfer(0x0, owner, INITIAL_SUPPLY);
   }
 
 }
